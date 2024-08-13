ec39a141b5a00b1ec0baf5b5b93d3d2c9e6312f4
Carlos Del Castillo M <maussac13@gmail.com>
2024-08-12 23:43:49 -0500

    add component & implement AddCategory

--- src/GifApp.jsx
+++ src/GifApp.jsx
@@ -1,7 +1,25 @@
+import { useState } from "react";
+import { AddCategory } from "./components/AddCategory";
 export const GifApp = () => {
+  const [categories, setCategories] = useState([
+    "Naruto shippuden",
+    "Dragon Ball",
+  ]);
+
+  const onAddCategory = (value) => {
+    if (categories.includes(value)) return;
+    setCategories([value, ...categories]);
+  };
+
   return (
     <>
       <h1>GifApp</h1>
+      <AddCategory onNewCategory={onAddCategory} />
+      <ol>
+        {categories.map((category) => {
+          return <ul key={category}>{category}</ul>;
+        })}
+      </ol>
     </>
   );
 };
