19a4dee1c5bdfd28073d3a38b2afda5d966dbdd7
Carlos Del Castillo M <maussac13@gmail.com>
2024-08-12 23:48:37 -0500

    updated

--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -10,7 +10,6 @@ export const AddCategory = ({ onNewCategory }) => {
   const onSubmit = (event) => {
     event.preventDefault();
     if (inputValue.trim().length <= 1) return;
-    // setCategories((categories) => [inputValue, ...categories]);
     onNewCategory(inputValue.trim());
     setInputValue("");
   };
