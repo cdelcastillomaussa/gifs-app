8f2d1d1cc2d96a248b24eb40209e4ef8c5787742
Carlos Del Castillo M <maussac13@gmail.com>
2024-08-18 23:21:43 -0500

    updated api-key sample dev

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,6 +1,6 @@
 export const getGifs = async (category) => {
-  const APY_KEY = "2pVs7ElNRHJ2Wj9JlLuEL6dJow5FoosT";
-  const url = `https://api.giphy.com/v1/gifs/search?api_key=${APY_KEY}&q=${category}&limit=10`;
+  const API_KEY = "6wo4Fzt6vdtz243QuWaZpMTVsfC8x9vl";
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${category}&limit=10`;
   const response = await fetch(url);
   const { data } = await response.json();
   const gifs = data.map((img) => ({
