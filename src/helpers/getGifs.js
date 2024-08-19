aea462449c33d3f5b8fac1c269fabd34ab9685c9
Carlos Del Castillo M <maussac13@gmail.com>
2024-08-18 21:23:14 -0500

    remove console.log

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,6 +1,6 @@
 export const getGifs = async (category) => {
-  const APY_KEY = "0rO3KmGaPvR2F860nfMJu9W5qzNo4gTT";
-  const url = `https://api.giphy.com/v1/gifs/search?api_key=${APY_KEY}&q=${category}&limit=20`;
+  const APY_KEY = "2pVs7ElNRHJ2Wj9JlLuEL6dJow5FoosT";
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=${APY_KEY}&q=${category}&limit=10`;
   const response = await fetch(url);
   const { data } = await response.json();
   const gifs = data.map((img) => ({
@@ -8,6 +8,5 @@ export const getGifs = async (category) => {
     title: img.title,
     url: img.images?.downsized_medium?.url,
   }));
-  console.log(gifs);
   return gifs;
 };
